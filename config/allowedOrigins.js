7fa4471614dd3ee0389e97b25ce6f27a3621596f
fangqiank <fangqiank@yahoo.com>
2022-09-10 09:18:16 +0800

    ready for deploy

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,5 +2,5 @@ export const allowedOrigins = [
   // 'http://localhost:3000',
   // 'http://127.0.0.1:3000',
   'http://localhost:3001',
-  'https://mern-technotes-frontend-3nlwg09io-messeneger.vercel.app/',
+  'https://mern-technotes-frontend.vercel.app/',
 ]
